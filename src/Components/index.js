dff645e3e514573a865139163a6c2784f8539490
Raul Dionisio <raldionisiosh@gmail.com>
2019-12-31 11:54:27 -0300

    Adding the search component

---
+++ src/Components/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css';
+
+const Search = () => (
+    <div className="search">
+        <h1>Busca Usuários</h1>
+        <input placeholder="Insira seu usuario do Gitihub" ></input>
+    </div>
+);
+
+export default Search;
